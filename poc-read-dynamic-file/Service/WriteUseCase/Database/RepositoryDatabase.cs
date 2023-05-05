e22b421f35415c84ab1958ebb3ea20059a623367
Alex Alves <alexalves2501@hotmail.com>
2023-05-05 16:08:40 -0300

    include benchmark annotations

--- poc-read-dynamic-file/Service/WriteUseCase/Database/RepositoryDatabase.cs
+++ poc-read-dynamic-file/Service/WriteUseCase/Database/RepositoryDatabase.cs
@@ -1,9 +1,11 @@
-﻿using Dapper;
+﻿using BenchmarkDotNet.Attributes;
+using Dapper;
 using poc_read_dynamic_file.Models;
 using System.Data;
 
 namespace poc_read_dynamic_file.Service.WriteUseCase.Database;
 
+[MemoryDiagnoser]
 public class RepositoryDatabase : IDisposable
 {
     private readonly DbContext _dbContext;
@@ -13,16 +15,19 @@ public class RepositoryDatabase : IDisposable
         _dbContext= new DbContext();
     }
 
+    [Benchmark]
     public async Task<IEnumerable<UserModel>> RecoverDataWithPipelineAsync()
     {
         var command = new CommandDefinition(@"SELECT * FROM [pocFile].[dbo].[User]", flags: CommandFlags.Pipelined);
         return await _dbContext.Connection.QueryAsync<UserModel>(command);
     }
 
+    [Benchmark]
     public async Task<IEnumerable<UserModel>> RecoverDataAsync()
         => await _dbContext.Connection.QueryAsync<UserModel>(
             sql: @"SELECT * FROM [pocFile].[dbo].[User]", new[] { typeof(UserModel) }, default, buffered: false);
 
+    [Benchmark]
     public IEnumerable<UserModel> RecoverData()
         => _dbContext.Connection.Query<UserModel>(
             sql: @"SELECT * FROM [pocFile].[dbo].[User]", buffered: false);
